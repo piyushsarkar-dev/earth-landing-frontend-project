bf5c2049596847531d75fc39681150475bfdc6ff
Piyush Sarkar <hi.mrpiyush@gmail.com>
2026-01-13 02:00:47 +0530

    About page Is Now Ready to use
    Updated the About page to include a banner section, mission statement with image, and integrated the MainPostCard component. Improved visual structure and added descriptive content to enhance user engagement.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,41 @@
+import MainPostCard from "@/components/MainPostCard";
+import Image from "next/image";
+
 const page = () => {
 	return (
-		<div className="grid min-h-screen place-items-center text-5xl">
-			{" "}
-			Welcome To About Page
-		</div>
+		<>
+			<section className="grid place-items-center bg-[url(/about/about-banner.jpg)] bg-cover bg-center py-64">
+				<h1 className="text-earth-dark text-7xl font-semibold tracking-widest">
+					WHO ARE WE?
+				</h1>
+			</section>
+
+			<section className="grid grid-cols-2 gap-4 px-48 py-28">
+				<Image
+					src="/about/our-mission.jpg"
+					alt="logo"
+					width={538}
+					height={401}
+					className="h-auto w-auto"
+				/>
+
+				<div className="space-y-4 p-16">
+					<div className="text-4xl font-semibold">OUR MISSION</div>
+
+					<div className="text-justify text-lg">
+						Hello, my name is Tyler Moore and with the help of many people I
+						made this template. I made it so it is super easy to update and so
+						that it flows perfectly with my tutorials. Lots of love and hundreds
+						of hours went into making it. I hope you love it as much as I do.
+					</div>
+
+					<div className="text-justify text-lg">
+						I wish you the best of luck with your business, enjoy the adventure.
+					</div>
+				</div>
+			</section>
+			<MainPostCard />
+		</>
 	);
 };
 
